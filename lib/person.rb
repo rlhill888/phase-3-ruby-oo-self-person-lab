29e6adaee370e1f6cfb3c5f61ff3a388ba6f04c2
RodneyHill <rlhill888@gmail.com>
2022-02-02 23:33:55 -0500

    commit tree f9f3fa9d6591d105414ac847457114951b1c420f

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,67 @@
 # your code goes here
+require 'pry'
+class Person
+
+    attr_reader :name
+
+    def initialize(name, bank_account= 25, happiness= 8, hygiene= 8)
+        @name=name
+        @bank_account=bank_account
+        @happiness=happiness
+        @hygiene=hygiene
+    end
+    def bank_account
+        @bank_account
+    end
+    def name
+        @name
+    end 
+    def bank_account= value
+        @bank_account= value
+    end
+    def happiness
+        @happiness
+    end
+    def happiness= value
+        unless value > 10 || value < 0
+            @happiness= value
+        end 
+    end 
+    def hygiene
+        @hygiene
+    end
+    def hygiene= value
+        unless value >10 || value < 0
+            @hygiene= value 
+        end 
+    end 
+
+    def clean?
+        if @hygiene >= 7
+            true 
+        else 
+            false 
+        end 
+    end
+    def happy?
+        if @happiness >= 7
+            true 
+        else 
+            false 
+        end 
+    end
+    def get_paid amount
+        puts "all about the benjamins"
+
+   
+
+    
+end 
+
+
+rl= Person.new('Rod')
+binding.pry
+
+    
+
+
